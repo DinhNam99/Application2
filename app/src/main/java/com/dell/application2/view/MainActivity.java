1773388695f1863b7e60470655266a941f015190
Sukio99ns <dinhnamnghiem@gmail.com>
2019-10-24 17:09:55 +0700

    Update MainActivity.java

--- app/src/main/java/com/dell/application2/view/MainActivity.java
+++ app/src/main/java/com/dell/application2/view/MainActivity.java
@@ -142,4 +142,6 @@ public class MainActivity extends AppCompatActivity implements ViewInterface.Mai
         super.onDestroy();
         EventBus.getDefault().unregister(this);
     }
+
+    //helo
 }
